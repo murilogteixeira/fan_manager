2cb93bf2d7c0adbb9b3aeab541871ef45b41e787
Murilo Teixeira <murilogteixeira@gmail.com>
2023-08-16 12:46:54 -0300

    feat: Initial commit

---
+++ fan_manager.ino
@@ -0,0 +1,201 @@
+#include <Arduino.h>
+#include <WiFiManager.h>
+#include <OTA.h>
+#include <Configuration.h>
+#include <EspFile.h>
+#include <Relay.h>
+#include <InputPin.h>
+
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ArduinoJson.h>
+
+#define RELAY1 D1
+#define RELAY2 D2
+#define SENSOR D3
+
+String host = "fan_manager";
+char ssid[] = "MURILO";
+char password[] = "94658243";
+const float maxTemp = 31;
+const int oneMinute = 1000 * 60;
+const int tempCheckInterval = oneMinute;
+const int activeTimeWhenIsOn = oneMinute * 3;
+const int checkInterval = 1000;
+
+unsigned long lastCheckTemp = checkInterval;
+unsigned long lastTempMillis = tempCheckInterval;
+unsigned long lastActiveRelayTimes[] = { -1, -1 };
+
+WiFiManager wifi;
+OTA ota;
+EspFile espFile;
+Configuration config;
+ESP8266WebServer server;
+OneWire oneWire(SENSOR);
+DallasTemperature sensors(&oneWire);
+
+Relay relay1(RELAY1, LOW, true);
+Relay relay2(RELAY2, LOW, true);
+Relay relays[] = { relay1, relay2 };
+
+DynamicJsonDocument statusJson(1024);
+
+void setupConfig();
+void setupConnection();
+void setupServer();
+void handleTemperatures();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("\n\nBooting...");
+  LittleFS.begin();
+  setupConfig();
+  setupConnection();
+  sensors.begin();
+}
+
+void loop() {
+  server.handleClient();
+  MDNS.update();
+  wifi.refreshConnection();
+  handleTemperatures();
+}
+
+void setupConfig() {
+  config.begin();
+
+  String espName = config.getEspName();
+
+  if (espName != "null" && espName != "" && espName != NULL)
+    host = espName;
+  else
+    config.setEspName(host);
+}
+
+void setupConnection() {
+  MDNS.begin(host);
+  wifi.connectStation(ssid, password, []() {
+    setupServer();
+    MDNS.addService("http", "tcp", 80);
+    Serial.printf("\nMDNS: http://%s.local\n", host.c_str());
+  });
+}
+
+void setupServer() {
+  server.enableCORS(true);
+
+  // OTA
+  server.on("/ota/update", HTTP_GET, []() {
+    server.send(200, "text/html", espFile.getUploadFileHtml());
+  });
+
+  server.on(
+    "/ota/update", HTTP_POST, []() {
+      ota.updateHandler(&server);
+    },
+    []() {
+      ota.uploadHandler(&server);
+    });
+
+  // CONFIG
+  server.on("/config/updateName", HTTP_GET, []() {
+    config.updateNameHandler(&server);
+  });
+
+  // FILE
+  server.on(espFile.getUploadPath(), HTTP_GET, []() {
+    server.send(200, "text/html", espFile.getUploadFileHtml());
+  });
+
+  server.on(
+    espFile.getUploadPath(), HTTP_POST, []() {
+      server.send(200);
+    },
+    []() {
+      espFile.uploadFileHandler(&server);
+    });
+
+  server.on(espFile.getDeletePath(), HTTP_GET, []() {
+    espFile.deleteFileHandler(&server);
+  });
+
+  server.on(espFile.getListPath(), HTTP_GET, []() {
+    espFile.listFileHandler(&server);
+  });
+
+  // RELAY
+  // server.on("/relay", []() {
+  //   relay.toggleHandler(&server);
+  // });
+
+  server.on("/relayOff", []() {
+    relay1.on();
+    relay2.on();
+    server.send(200);
+  });
+
+  server.on("/relayOn", []() {
+    relay1.off();
+    relay2.off();
+    server.send(200);
+  });
+
+  server.on("/status", []() {
+    statusJson["relays"]["0"] = relay1.getState();
+    statusJson["relays"]["1"] = relay2.getState();
+
+    String statusString;
+    serializeJson(statusJson, statusString);
+    server.send(200, "application/json", statusString.c_str());
+  });
+
+  server.on("/", []() {
+    server.send(200, "text/html", "");
+  });
+
+  // 404
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "404: Not Found");
+  });
+
+  server.begin();
+}
+
+void handleTemperatures() {
+  // Request temperatures
+  if (millis() - lastCheckTemp < checkInterval) return;
+  lastCheckTemp = millis();
+
+  Serial.print("Requesting temperatures... ");
+  sensors.requestTemperatures();
+  Serial.println("Ready!");
+
+  // Get devices count
+  int deviceCount = sensors.getDeviceCount();
+  float temp[deviceCount];
+
+  // Save the current temperatures
+  for (int i = 0; i < deviceCount; i++) {
+    temp[i] = sensors.getTempCByIndex(i);
+    Serial.printf("Sensor %d - Temp: %.2f\n", i + 1, temp);
+    statusJson["sensors"][String(i)] = temp[i];
+  }
+
+  // Update relay states
+  if (millis() - lastTempMillis < tempCheckInterval) return;
+  lastTempMillis = millis();
+
+  for (int i = 0; i < deviceCount; i++) {
+    if (temp[i] > maxTemp) {
+      relays[i].on();
+      if (lastActiveRelayTimes[i] < 0)
+        lastActiveRelayTimes[i] = millis();
+    } else if (millis() - lastActiveRelayTimes[i] > activeTimeWhenIsOn) {
+      relays[i].off();
+      lastActiveRelayTimes[i] = -1;
+    }
+  }
+}
